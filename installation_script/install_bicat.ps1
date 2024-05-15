eafaadf031a2c8b167a12141d039923fc29a825b
huahuadeliaoliao <huahuadeliaoliao@icloud.com>
2024-05-15 18:43:56 +0800

    change ps1

--- installation_script/install_bicat.ps1
+++ installation_script/install_bicat.ps1
@@ -1,14 +1,9 @@
 $ErrorActionPreference = "Stop"
 
-$REPO = "huahuadeliaoliao/biCat"
-$VERSION = "v0.1.0"
 $BINARY_NAME = "bicat"
+$URL = "https://github.com/huahuadeliaoliao/biCat/releases/download/v0.1.0/bicat.exe"
 $TMP_DIR = "$env:TEMP"
 $INSTALL_DIR = "$env:LocalAppData\Programs"
-
-$ARCH = if ([System.Environment]::Is64BitOperatingSystem) { "amd64" } else { "x86" }
-
-$URL = "https://github.com/$REPO/releases/download/$VERSION/$BINARY_NAME-windows-$ARCH.exe"
 $TMP_FILE = "$TMP_DIR\$BINARY_NAME.exe"
 
 Write-Output "Downloading $BINARY_NAME from $URL"
@@ -22,7 +17,7 @@ Move-Item -Path $TMP_FILE -Destination "$INSTALL_DIR\$BINARY_NAME.exe"
 
 # 检查并添加到 PATH 环境变量
 $path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
-if ($path -notcontains $INSTALL_DIR) {
+if ($path -notlike "*$INSTALL_DIR*") {
     [System.Environment]::SetEnvironmentVariable("Path", "$path;$INSTALL_DIR", [System.EnvironmentVariableTarget]::User)
     Write-Output "$INSTALL_DIR added to PATH"
 } else {
